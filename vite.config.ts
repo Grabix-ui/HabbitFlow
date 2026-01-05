39355f8e4bf54d41fa85ce146f195057430aedbd
Grabix-ui <todorokigabi18@gmail.com>
2026-01-05 16:33:33 +0100

    Change base path for asset loading in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   // Ustawienie './' sprawia, że ścieżki do zasobów (JS, CSS) są relatywne
-  base: './',
+  base: '/HabbitFlow/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
