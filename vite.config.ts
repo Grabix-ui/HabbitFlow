fed9ab2fa63851ff1efe801d4abbc540404f7f11
Grabix-ui <todorokigabi18@gmail.com>
2026-01-05 13:52:13 +0100

    Change base path to relative in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Ustawienie base na nazwę Twojego repozytorium na GitHubie
-  base: '/HabbitFlow/',
+  // Ustawienie './' sprawia, że wszystkie ścieżki są relatywne
+  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
