0bda3aba78e2a4811ffd587389597e0ed72dd01f
Grabix-ui <todorokigabi18@gmail.com>
2026-01-05 13:35:55 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // WAŻNE: To musi pasować DOKŁADNIE do nazwy Twojego repozytorium na GitHubie
+  // To musi być nazwa Twojego repozytorium na GitHubie pomiędzy ukośnikami
   base: '/HabbitFlow/', 
   build: {
     outDir: 'dist',
