78a10d272a210985105e5dd3710d550742d45636
Grabix-ui <todorokigabi18@gmail.com>
2026-01-05 13:48:49 +0100

    Update base path in Vite config for GitHub Pages
    Changed base path for GitHub Pages deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Używamy relatywnej ścieżki bazowej, co rozwiązuje większość problemów z GitHub Pages
-  base: './',
+  // Ustawienie base na nazwę Twojego repozytorium na GitHubie
+  base: '/HabbitFlow/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
