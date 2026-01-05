96bd6b38f59b597593ea29538421ed3e888d9542
Grabix-ui <todorokigabi18@gmail.com>
2026-01-05 13:57:46 +0100

    Improve comments in vite.config.ts
    Updated comments for clarity regarding base path.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Ustawienie './' sprawia, że wszystkie ścieżki są relatywne
+  // Ustawienie './' sprawia, że ścieżki do zasobów (JS, CSS) są relatywne
   base: './',
   build: {
     outDir: 'dist',
